7bb48f43de9cda5fe9b1656c0e31e62e84ff318f
Florin Iacob <florin.iacob@gmail.com>
2024-01-20 16:04:19 +0200

    Curs 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,35 @@ function App() {
   return (
     <div className="app">
 
+        <ul className="lista">
+            <li>mere</li>
+            <li>pere</li>
+            <li>gutui</li>
+            <li>prune</li>
+            <li>struguri</li>
+        </ul>
+        <ul className="lista speciala">
+            <li>mere</li>
+            <li className={"speciala"}>pere</li>
+            <li>gutui</li>
+            <li>prune</li>
+            <li>struguri</li>
+            <div>
+                <div className={"speciala"}>nepot special</div>
+            </div>
+        </ul>
+
+
+        <ul className="lista speciala">
+            <li>mere</li>
+            <li className={"speciala"}>pere</li>
+            <li>gutui</li>
+            <li>prune</li>
+            <li>struguri</li>
+            <div>
+                <div className={"speciala"}>nepot special</div>
+            </div>
+        </ul>
 
     </div>
   );
