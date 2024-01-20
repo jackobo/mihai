27507eac657cd8ee91bc5efd4845a4cc110c318a
Florin Iacob <florin.iacob@gmail.com>
2024-01-20 10:18:39 +0200

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-        <div className="box1">
-            <a href="https://www.youtube.com/" target="_blank">
-                Iutub
-            </a>
-        </div>
 
-        <div className="box2">
-            asfasf
-        </div>
-        <iframe src="https://888sparkware.ro/"/>
 
     </div>
   );
