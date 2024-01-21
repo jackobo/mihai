b7edee95f10b7d1dd271b04b39099a0a56bdbccc
Florin Iacob <florin.iacob@gmail.com>
2024-01-21 17:30:26 +0200

    Curs 3 (flexbox)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
+    return (
+        <div className="app">
+            <div className="header">
 
-    </div>
-  );
+            </div>
+            <div className="middle">
+                <div className="sidemenu">
+
+                </div>
+                <div className="content">
+                    <div className={"content-data"}>
+                        sdfasdfasdfadsfads
+                    </div>
+                </div>
+
+            </div>
+            <div className="footer">
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
